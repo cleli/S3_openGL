856e1145b23c03f03ba72eecf4acbc7ed39f0f83
cleli <cleliechassignet@hotmail.fr>
2019-12-16 19:27:02 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ GLImac-BuildTemplate/CMakeFiles/opengl-tutorials.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl-tutorials.dir/src/app.cpp.o"
+  "CMakeFiles/opengl-tutorials.dir/src/gl-exception.cpp.o"
+  "CMakeFiles/opengl-tutorials.dir/src/main.cpp.o"
+  "CMakeFiles/opengl-tutorials.dir/src/shader.cpp.o"
+  "bin/opengl-tutorials"
+  "bin/opengl-tutorials.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl-tutorials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
