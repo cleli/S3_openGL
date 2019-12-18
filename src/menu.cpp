3923a23da21670f79c6fb328e16fd835761f698f
cleli <cleliechassignet@hotmail.fr>
2019-12-18 17:16:56 +0100

    afficher la couleur sélectionnée

--- src/menu.cpp
+++ src/menu.cpp
@@ -12,6 +12,8 @@
 #include "menu.hpp"
 #include "cube.h"
 
+std::string current_color_string="Violet";
+
 static bool rouge=false;
 static bool orange=false;
 static bool jaune=false;
@@ -25,7 +27,7 @@ void afficheMenu(unsigned int curseur, glm::vec4* current_color, Cube stockCube[
     ImGui::Begin("Menu");
 
     //Menu couleurs
-    if (ImGui::BeginMenu("Couleur"))
+    if (ImGui::BeginMenu("Couleur : "))
     {
         ImGui::MenuItem("Rouge", NULL, &rouge);
         ImGui::MenuItem("Orange", NULL, &orange);
@@ -37,41 +39,52 @@ void afficheMenu(unsigned int curseur, glm::vec4* current_color, Cube stockCube[
         ImGui::EndMenu();
     }
 
+    ImGui::Text("%s", current_color_string.c_str());
+
     if (rouge==true){
         *current_color = glm::vec4(1.0f, 0.0f, 0.0f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Rouge";
         rouge=false;
     }
     if (orange==true){
         *current_color = glm::vec4(1.0f, 0.6f, 0.0f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Orange";
         orange=false;
     }
     if (jaune==true){
         *current_color = glm::vec4(1.0f,0.89f, 0.2f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Jaune";
         jaune=false;
     }
     if (vert==true){
         *current_color = glm::vec4(0.0f,1.0f, 0.0f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Vert";
         vert=false;
     }
     if (indigo==true){
         *current_color = glm::vec4(0.47f,0.1f, 0.97f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Indigo";
         indigo=false;
     }
     if (bleu==true){
         *current_color = glm::vec4(0.0f,0.0f, 1.0f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Bleu";
         bleu=false;
     }
     if (violet==true){
         *current_color = glm::vec4(0.93f,0.5f, 0.93f, 0.8f);
         if(stockCube[curseur].isVisible==true) stockCube[curseur].color= *current_color;
+        current_color_string="Violet";
         violet=false;
     }
+    ImGui::Spacing();
+    ImGui::Spacing();
 
     //Sculpture du monde : créer, supprimer, creuser et extruder
 
